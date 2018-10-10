63930c0c4e3ad10d295143b710daefbf9ce82cb9
Sumit1602 <sumitchaudhary39@gmail.com>
2018-10-10 18:10:07 +0530

    Q1 First commit

---
+++ src/main/groovy/Q1_Person.groovy
@@ -0,0 +1,23 @@
+
+println "Q1. Create a class in Java along with follwing fields. classname: Person fields: name, age, gender, address Access the fields in all known ways: like through getter , by dot operator"
+
+class Q1Person {
+    String name;
+    int age;
+    String gender;
+    String address;
+}
+Q1Person person = new Q1Person();
+person.setName("Sumit")
+println person.getName()
+
+person.setAge(23)
+println person.getAge()
+
+person.setGender("Male")
+println person.getGender()
+
+person.setAddress("Mayur vihar phase-1, Delhi-110091")
+println person.getAddress()
+
+println "Name: " + person.name + ", Age: "+ person.age + ", Gender: " + person.gender + ", Address: "+ person.address
