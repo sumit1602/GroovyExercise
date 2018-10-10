d780be1644dad21b6f1952489fb14a5722740883
Sumit1602 <sumitchaudhary39@gmail.com>
2018-10-10 18:25:44 +0530

    Q3 First commit

---
+++ src/main/groovy/Q3.groovy
@@ -0,0 +1,16 @@
+//Q3.Print this pattern using groovy:
+//
+//
+//    *
+//    **
+//    ****
+//    ********
+
+
+1.times {print '*' }
+println ""
+2.times {print '*' }
+println ""
+4.times {print '*' }
+println ""
+8.times {print '*' }
