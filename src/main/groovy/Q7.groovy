b82889904ad500a39c1f159a8a10c23c72fbd632
Sumit1602 <sumitchaudhary39@gmail.com>
2018-10-12 15:38:13 +0530

    Q7 2nd Commit

--- src/main/groovy/Q7.groovy
+++ src/main/groovy/Q7.groovy
@@ -1,1 +1,7 @@
+//Q7. Write a closure which checks if a value is contained within a list where the closure accepts two parameters
+List li =[1,2,3,4,5,6]
+
+//li.each{println it}
+li.eachWithIndex{a, i -> if(a==4) println a+" is present" }
+
 
