585599e2382729c3712af431c050a8803a856f97
Sumit1602 <sumitchaudhary39@gmail.com>
2018-10-12 17:04:31 +0530

    Q8 2nd Commit

--- src/main/groovy/Q8.groovy
+++ src/main/groovy/Q8.groovy
@@ -1,1 +1,10 @@
+//Q8. Combine content of all the files in a specific directory to another new file
+
+//new File('/home/sumit/Desktop/fromCopy/file2').text = new File('/home/sumit/Desktop/toCopy/fileToCopy').text
+
+//def src = new File('/home/sumit/Desktop/fromCopy').eachFile {line -> println "line: $line"}
+
+def destination = new File('/home/sumit/Desktop/toCopy/file3')
+new File('/home/sumit/Desktop/fromCopy').eachFileRecurse{file-> destination<<(file.getText())}
+
 
